58ed610110fb9262edeafe98f1ec3afc05a87b43
Nana Yang <nana.hy.yang@gmail.com>
2024-09-19 16:47:16 +0800

    feat: 更新框架 + 前後端分離 1. 前端使用 vite-react-typescript 2. 嵌入 Tally 表單到 Form page 中

---
+++ react-app/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Mainpage from "./pages/Mainpage.tsx";
+import Form from "./pages/Form.tsx";
+
+function App() {
+
+  return (
+      <>
+          <Mainpage/>
+          <Form/>
+      </>
+  )
+}
+
+export default App
---
+++ react-app/src/pages/Mainpage.tsx
@@ -0,0 +1,11 @@
+function Mainpage() {
+
+    return (
+        <>
+            <h1>Week 3 評分表單</h1>
+        </>
+    )
+}
+
+
+export default Mainpage;
