fb8b9d6eee7bf41c323a9b880a25fd79552f47c6
Nana Yang <nana.hy.yang@gmail.com>
2024-10-05 17:51:07 +0800

    chore: 將apps script移動到googleForm目錄底下

---
+++ back-end/src/googleForm/handleResponse/deleteEntry.js
@@ -0,0 +1,24 @@
+const { google } = require("googleapis");
+function deleteEntry(invalidRow, columnRange, spreadsheetId, auth) {
+    let newLetter = String.fromCharCode("A".charCodeAt(0) + columnRange - 1);
+    let rangeToDelete = "A" + invalidRow + ":" + newLetter + invalidRow;
+
+    const sheet = google.sheets({ version: "v4", auth });
+
+
+    sheet.spreadsheets.values.clear({
+        spreadsheetId: spreadsheetId,
+        range: rangeToDelete      // 要清除的範圍，例如 A7:K7
+    }, (err, res) => {
+        if (err) {
+            console.error("The API returned an error: " + err);
+            return;
+        }
+        console.log(`Range ${rangeToDelete} cleared successfully.`);
+    });
+
+
+}
+//要改
+//好像只能清資料，不能刪整行
+module.exports = { deleteEntry };
