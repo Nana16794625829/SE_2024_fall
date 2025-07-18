5241bcc372a1f9b25e2e49f1cd8d7d65c9085fcc
Nana Yang <nana.hy.yang@gmail.com>
2025-07-18 14:35:20 +0800

    chore: reorganize project folder to better clarity

--- apps/react-app/src/App.tsx
+++ apps/react-app/src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css'
 import Mainpage from "./pages/Mainpage.tsx";
-import Form from "./pages/Form.tsx";
+// import Form from "./pages/Form.tsx";
 
 function App() {
 
   return (
       <>
           <Mainpage/>
-          <Form/>
+          {/*<Form/>*/}
       </>
   )
 }
